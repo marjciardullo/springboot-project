053762b8d81dd668c2e23e84d7ad1646992684a7
Julia Gato <72545800+juliadelua@users.noreply.github.com>
2021-10-24 17:56:03 -0300

    base repository

---
+++ cineset/cineset/src/main/java/com/cineset/cineset/CinesetApplication.java
@@ -0,0 +1,13 @@
+package com.cineset.cineset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinesetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinesetApplication.class, args);
+	}
+
+}
